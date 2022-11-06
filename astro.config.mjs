34597538c8987da021227768e3acaa00344299c4
Keenan Nicholson <keenan@pop-os.localdomain>
2022-11-05 21:54:10 -0230

    launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
-export default defineConfig({});
+
+
+export default {
+  // ...
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  site: 'https://Keenan-Nicholson.github.io',
+  base: '/Keenan-Nicholson/atro-website'
+}
