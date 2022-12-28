62480bc6075aa7da122138f3c4bb8b2e89fa9f79
Keenan Nicholson <Keenanbnicholson@gmail.com>
2022-12-28 16:40:31 -0330

    About/Portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 
-
 export default {
   // ...
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
-  site: 'https://Keenan-Nicholson.github.io'
-  
-}
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  site: "https://keenannicholson.me",
+};
